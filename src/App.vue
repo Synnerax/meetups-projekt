a9cfc1c5635d20f8db5f263ffee2b5a49947984c
jensengbg-elliot-berggren <elliot.berggren@hotmail.com>
2021-02-24 21:05:49 +0100

    added light css on event cards added api call on mount and prop down data to product card

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,46 @@
   <!--<div id="nav">
     <router-link to="/">Home</router-link>
   </div> -->
-  <router-view/>
+  <router-view  :eventsArray="eventsArray"/>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      eventsArray: []
+    }
+  },
+  methods: {
+    async getEventDataArray() {
+      let settings = {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Master-Key': '$2b$10$IQutUOnIDU5m1VTI.4PzQ.M1ZzdQ4Q/XZzMz/MT7RKqX8oHx3k0pu',
+            'X-Bin-Versioning': 'false'
+        }
+      }
+      try{
+      let resp = await fetch(`https://api.jsonbin.io/v3/b/60355a8d0866664b10820263/latest`, settings)
+      let data = await resp.json()
+
+      this.eventsArray = await data.record.Events
+      console.log(data)
+      }
+      catch(err){
+        console.error(err)
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      console.log("i was created")
+      this.getEventDataArray()
+    })
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
